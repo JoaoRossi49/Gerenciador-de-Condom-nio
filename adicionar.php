2ceab78080fd8d034c3475d5c9945c24838fbf38
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2022-09-17 20:39:54 -0300

    first commit

---
+++ adicionar.php
@@ -0,0 +1,15 @@
+<h1>Adicionar usuário</h1>
+<form action="adicionar_action.php" method="post">
+  <label>
+    Nome:
+    <input type="text" name="name">
+  </label> <br/><br/>
+
+  <label>
+    Email:
+    <input type="email" name="email">
+  </label> <br/><br/>
+
+  <input type="submit" value="Adicionar">
+
+</form>
